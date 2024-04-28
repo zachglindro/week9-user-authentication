bf9fadaeaaf350bea2e71cac8e86e35a55a3351e
abigailnadua <acnadua@up.edu.ph>
2024-04-28 19:44:02 +0800

    initial files

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+/*
+  Created by: Claizel Coubeili Cepe
+  Date: updated April 26, 2023
+  Description: Sample todo app with Firebase 
+*/
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'firebase_options.dart';
+import 'pages/todo_page.dart';
+import 'providers/todo_provider.dart';
+import 'providers/auth_provider.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: ((context) => TodoListProvider())),
+        ChangeNotifierProvider(create: ((context) => UserAuthProvider()))
+      ],
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SimpleTodo',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const TodoPage(),
+      },
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
---
+++ lib/pages/todo_page.dart
@@ -0,0 +1,135 @@
+/*
+  Created by: Claizel Coubeili Cepe
+  Date: updated April 26, 2023
+  Description: Sample todo app with Firebase 
+*/
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../models/todo_model.dart';
+import '../providers/todo_provider.dart';
+import '../providers/auth_provider.dart';
+import 'user_details_page.dart';
+import 'modal_todo.dart';
+
+class TodoPage extends StatefulWidget {
+  const TodoPage({super.key});
+
+  @override
+  State<TodoPage> createState() => _TodoPageState();
+}
+
+class _TodoPageState extends State<TodoPage> {
+  @override
+  Widget build(BuildContext context) {
+    // access the list of todos in the provider
+    Stream<QuerySnapshot> todosStream = context.watch<TodoListProvider>().todo;
+
+    return Scaffold(
+        drawer: drawer,
+        appBar: AppBar(
+          title: const Text("Todo"),
+        ),
+        body: StreamBuilder(
+          stream: todosStream,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text("Error encountered! ${snapshot.error}"),
+              );
+            } else if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (!snapshot.hasData) {
+              return const Center(
+                child: Text("No Todos Found"),
+              );
+            }
+
+            return ListView.builder(
+              itemCount: snapshot.data?.docs.length,
+              itemBuilder: ((context, index) {
+                Todo todo = Todo.fromJson(
+                    snapshot.data?.docs[index].data() as Map<String, dynamic>);
+                todo.id = snapshot.data?.docs[index].id;
+                return Dismissible(
+                  key: Key(todo.id.toString()),
+                  onDismissed: (direction) {
+                    context.read<TodoListProvider>().deleteTodo(todo.title);
+
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('${todo.title} dismissed')));
+                  },
+                  background: Container(
+                    color: Colors.red,
+                    child: const Icon(Icons.delete),
+                  ),
+                  child: ListTile(
+                    title: Text(todo.title),
+                    leading: Checkbox(
+                      value: todo.completed,
+                      onChanged: (bool? value) {
+                        context
+                            .read<TodoListProvider>()
+                            .toggleStatus(todo.id!, value!);
+                      },
+                    ),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) => TodoModal(
+                                type: 'Edit',
+                                item: todo,
+                              ),
+                            );
+                          },
+                          icon: const Icon(Icons.create_outlined),
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) => TodoModal(
+                                type: 'Delete',
+                                item: todo,
+                              ),
+                            );
+                          },
+                          icon: const Icon(Icons.delete_outlined),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              }),
+            );
+          },
+        ));
+  }
+
+  Drawer get drawer => Drawer(
+          child: ListView(padding: EdgeInsets.zero, children: [
+        ListTile(
+          title: const Text('Details'),
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const UserDetailsPage()));
+          },
+        ),
+        ListTile(
+          title: const Text('Logout'),
+          onTap: () {
+            context.read<UserAuthProvider>().signOut();
+            Navigator.pop(context);
+          },
+        ),
+      ]));
+}
